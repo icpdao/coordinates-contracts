0ed546ead4d38f4275392f1d1b186e02f641a02e
fushang318 <fushang318@gmail.com>
2021-11-16 21:08:11 +0800

    feat: change deploy script

--- deploy/00_deploy_my_contract.ts
+++ deploy/00_deploy_my_contract.ts
@@ -7,11 +7,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const {deploy} = deployments;
 
     const {deployer} = await getNamedAccounts();
-    const owner = "0xAce84e2A50EfcF847c3a1d21018cecc2075E4a78";
+    const owner = "0x181b12729cCB398a7151635e7a61Fc9EDD3369Bb";
+
+    const startUp = "0xAce84e2A50EfcF847c3a1d21018cecc2075E4a78";
 
     await deploy('LootLand', {
         from: deployer,
-        args: [owner],
+        args: [owner, startUp],
         log: true,
         gasPrice: BigNumber.from(10).pow(9).mul(80)
     });
